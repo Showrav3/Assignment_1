250e5b82ad0401b4990b61d79f09a1daa4fcc6b7
Showrav Barua <34981610+Showrav3@users.noreply.github.com>
2024-07-06 14:23:17 +0600

    Assignment done
    1. Create a Personal Profile Card
    2. Find the largest number using PHP
    3. Celsius to Fahrenheit Conversion

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,7 @@
+<?php
+    $celsius = 32;
+    $fahrenheit = ($celsius * 9/5) + 32; //Convert Celsius to Fahrenheit
+
+    printf("The temperature 32 degrees celsius is equal to %.2f degrees fahrenheit.", $fahrenheit);
+?>
+
---
+++ largest_number.php
@@ -0,0 +1,23 @@
+<?php
+    // Given numbers
+    $num1 = 4;
+    $num2 = 5;
+    $num3 = 6;
+
+    // Find the largest number using if-else statements
+    if ($num1 > $num2 && $num1 > $num3)
+    {
+        echo "The largest number is $num1.";
+    }
+    else
+    {
+        if ($num2 > $num3)
+        {
+            echo "The largest number is $num2.";
+        }
+        else
+        {
+            echo "The largest number is $num3.";
+        }
+    }
+?>
